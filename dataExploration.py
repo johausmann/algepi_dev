fb8fcafb711fb898a6723f849ee80b409392326e
LKress <luis.kress@web.de>
2022-06-16 12:41:08 +0200

    Update 20220616: * cleaned the model training and data exploration code * added initial pytorch net

--- dataExploration.py
+++ dataExploration.py
@@ -1,10 +1,48 @@
+from sklearn import preprocessing
 import seaborn as sns
 import pandas as pd
+import numpy as np
+
+numeric_cols = ["MNase_TSSm150", "H3K27me3_TSSm150", "H3K4me3_TSSm150", 
+                "H3K9ac_TSSm150", "Pol2_TSSm150", "MNase_TSSp300", 
+                "H3K27me3_TSSp300", "H3K4me3_TSSp300", "H3K9ac_TSSp300", 
+                "Pol2_TSSp300", "MNase_TTSm200", "H3K27me3_TTSm200", 
+                "H3K4me3_TTSm200", "H3K9ac_TTSm200", "Pol2_TTSm200", 
+                "MNase_GB", "H3K27me3_GB", "H3K4me3_GB", "H3K9ac_GB", 
+                "Pol2_GB", "IntergenicLen", "GeneLen", "sRNA", "GC", 
+                "IntronFreq", "mRNA"]
+
+def plot_distribution(data, cols):
+    for col in cols:
+        plot = sns.displot(data, x=col, kind="kde", fill=True)
+        plot.set_titles("{col} distribution")
+        plot.savefig(f"dist/{col}_distribution.png")
+
+def plot_transformed_distribution(data, cols):
+    for col in cols:
+        plot = sns.displot(data, x=col, kind="kde", fill=True)
+        plot.set_titles("{col} transformed distribution")
+        plot.savefig(f"dist/{col}_transformed_distribution.png")
+
+
+
 
 data = pd.read_csv("data.csv")
+# get only columns containing numeric values
+nv = data[numeric_cols].to_numpy()
 
-for col in data.iloc[:, 1:-2].columns.values:
-    plot = sns.scatterplot(data=data, x=col, y=list(data.columns.values)[-1])
-    fig = plot.get_figure()
-    fig.savefig(f"{col}_y.png")
-    fig.clear()
+# log2 transformation on the numeric columns
+log2_transform = lambda x: np.log2(x+1)
+transformer = preprocessing.FunctionTransformer(log2_transform)
+nv_transformed = transformer.transform(nv)
+
+# scaling of the previously transformed values
+scaler = preprocessing.StandardScaler().fit(nv_transformed)
+nv_preprocessed = scaler.transform(nv_transformed)
+
+trans_data = pd.DataFrame(nv_transformed, columns=numeric_cols)
+
+scaled_data = pd.DataFrame(nv_preprocessed, columns=numeric_cols)
+
+plot_distribution(data, numeric_cols)
+plot_transformed_distribution(data, numeric_cols)
